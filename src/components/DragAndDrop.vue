170bb652fe86d87ed40232216f595cc43bc83059
Salvatore Porto <salvatore.porto@student.hepl.be>
2021-04-30 17:42:13 +0200

    ok

--- src/components/DragAndDrop.vue
+++ src/components/DragAndDrop.vue
@@ -4,17 +4,21 @@
             <div class="col s12">
                 <div class="card-panel teal">
                     <div @dragover="dragover" @dragleave="dragleave" @drop="drop">
-                        <input type="file"  name="fields[assetsFieldHandle][]" id="assetsFieldHandle"
-                               @change="onChange" ref="file" accept=".csv,.xls,.xlsx" />
+                        <input type="file" name="FichierListe" id="assetsFieldHandle"
+                               @change="onChange" ref="file" accept=".csv,.xls,.xlsx"/>
 
-                        <label for="assetsFieldHandle" >
+                        <label for="assetsFieldHandle">
                             <div class="row">
-                                <h6> Glisser un fichier (*.csv,*.xls,*.xlsx) ou <h5>appuyer</h5> pour charger votre liste.</h6>
+                                <h6> Glisser un fichier (*.csv,*.xls,*.xlsx) ou <h5>appuyer</h5> pour charger votre
+                                    liste.
+                                </h6>
                             </div>
                         </label>
-                        <ul  v-if="this.filelist.length" v-cloak class="center">
-                            <li  v-for="file in filelist" v-bind:key="file">
-                                {{ file.name }}<button type="button" @click="remove(filelist.indexOf(file))" title="Remove file">x</button>
+                        <ul v-if="this.filelist.length" v-cloak class="center">
+                            <li v-for="file in filelist" v-bind:key="file">
+                                {{ file.name }}
+                                <button type="button" @click="remove(filelist.indexOf(file))" title="Remove file">x
+                                </button>
                             </li>
                         </ul>
                     </div>
@@ -37,11 +41,12 @@ export default {
         }
     },
     methods: {
-         upload(){
-            let file = this.$refs.filelist.files[0];
+        upload() {
+            console.log("fct upload lancer et il ne se passe rien!");
+            let file = this.$refs.file.files[0];
             let formData = new FormData();
-            formData.append(file);
-            axios.post("/api",formData)
+            formData.append("file", file);
+            axios.post("/Fichierliste", formData)
                 .catch(() => {
                     console.log("erreur");
                 });
@@ -82,23 +87,29 @@ export default {
 .file-wrapper input {
     position: absolute;
     top: 0;
-    right: 0; /* not left, because only the right part of the input seems to
-                 be clickable in some browser I can't remember */
+    right: 0;
+    /* not left, because only the right part of the input seems to
+                    be clickable in some browser I can't remember */
     cursor: pointer;
     opacity: 0.0;
-    filter: alpha(opacity=0); /* and all the other old opacity stuff you
-                                 want to support */
-    font-size: 300px; /* wtf, but apparently the most reliable way to make
-                         a large part of the input clickable in most browsers */
+    filter: alpha(opacity=0);
+    /* and all the other old opacity stuff you
+                                    want to support */
+    font-size: 300px;
+    /* wtf, but apparently the most reliable way to make
+                            a large part of the input clickable in most browsers */
     height: 200px;
 }
+
 [v-cloak] {
     display: none;
 }
-#assetsFieldHandle{
+
+#assetsFieldHandle {
     display: none;
 }
-h5,h6{
+
+h5, h6 {
     color: black;
     text-align: center;
 }
